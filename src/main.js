a51a962147e505910c94de0e954666cece1459fd
liz282907 <422364804@qq.com>
2017-05-16 10:09:55 +0800

    add partial memoize and wrap

--- src/main.js
+++ src/main.js
@@ -30,7 +30,71 @@ _.before = (count, fn) => {
         
         return result;
     }
-
 }
+
+/**
+ * Partially apply a function by creating a version that has had some 
+ * of its arguments pre-filled, without changing its dynamic this context. 
+ * _ acts as a placeholder, allowing any combination of arguments to be 
+ * pre-filled.
+ * @param  {Function}  fn   [description]
+ * @param  {...[type]} args [description]
+ * @return {[type]}         [description]
+ */
+_.partial = (fn,...args)=>{
+	return function(...leftArgs){
+		let fillCount = 0;
+		for(let i=0;i<args.length;i++){
+			if(args[i]===_) args[i] = leftArgs[fillCount++];
+		}
+		const totalArgs = args.concat(leftArgs.slice(fillCount));
+		return fn.apply(this,totalArgs)
+	}
+}
+
+_.once = (fn)=>{
+	return _.before(1,fn)
+}
+
+/**
+ * 将第一个函数 function 封装到函数 wrapper 里面, 
+ * 并把函数 function 作为第一个参数传给 wrapper. 
+ * 这样可以让 wrapper 在 function 运行之前和之后 执行代码, 
+ * 调整参数然后附有条件地执行.
+ * 本来return (...args)=>{
+ * 		const _args = [fn,...args];
+		return wrapper.apply(this,_args);
+	}
+	但实际上应该更有扩展性一点,可以复用partial的代码，即把fn作为参数缓存
+ * @param  {Function} fn      [description]
+ * @param  {[type]}   wrapper [description]
+ * @return {[type]}           [description]
+ */
+_.wrap = (fn,wrapper)=>{
+	return _.partial(wrapper,fn)
+}
+
+/**
+ * Memoizes方法可以缓存某函数的计算结果。返回一个函数fn(key)，持有cache属性（obj）,里面存放key: fn(key)的函数历史结果
+ * memoize的第二个参数是可选参数，用于计算cache里面的key,如果未传递，则默认使用key,即fn的第一个参数。
+ * 测试时，需要注意缓存结果跟计算结果要严格等于返回函数的cache属性。
+ * 除此之外，cache应该对每一个返回的fn独有，而不应该在2处。
+ * @param  {[type]} function       [description]
+ * @param  {[type]} [hashFunction] [description]
+ * @return {[type]}                [description]
+ */
+_.memoize = (function(){
+	let hashFunction = (...args)=>args[0];  //2
+	return (fn,hasher = hashFunction)=>{
+		const cache = {};
+		const memoize = (...args)=>{
+			const customKey = hasher.apply(this,args);
+			return cache[customKey] || (cache[customKey] = fn.apply(this,args))
+		}
+		memoize.cache = cache;
+		return memoize;
+	}
+})()
+
 
 export default _;
--- test/main.test.js
+++ test/main.test.js
@@ -28,49 +28,130 @@ describe('underscore/Function tests: ', function() {
 
     describe('2. _.after 函数：', function() {
         const notes = [1, 2, 3];
-        
+
         it('should be excuted until all have excuted ', function(done) {
-        	let count = 0;
-        	const render = ()=> {
-            	// console.log('---done!---')
-            	assert.equal(count,notes.length);
-            	done();
+            let count = 0;
+            const render = () => {
+                // console.log('---done!---')
+                assert.equal(count, notes.length);
+                done();
             }
             const renderNotes = _.after(notes.length, render);
-            notes.forEach((note)=>{
-            	const fn = ((note)=> {
-            		count++;
-            		// console.log('  note  ',note);
-            		renderNotes();
-            	}).bind(this,note);
-            	asyncFunction(fn)
-            });  
+            notes.forEach((note) => {
+                const fn = ((note) => {
+                    count++;
+                    // console.log('  note  ',note);
+                    renderNotes();
+                }).bind(this, note);
+                asyncFunction(fn)
+            });
         })
     })
 
     describe('3. _.before 函数：', function() {
-        const notes = [1, 2, 3],count = 6;
-        
+        const notes = [1, 2, 3],
+            count = 6;
+
         it('should return the same result after count', function() {
-        	let initial = 1000,step = 100;
-        	const askForRaise = (function(initial,step){
-        		let salary = initial;
-        		return ()=> {
-        			salary += step;
-        			return salary;
-        		};
-        	})(initial,step);
-        	const raiseSalaryBy100 = _.before(count,askForRaise);
+            let initial = 1000,
+                step = 100;
+            const askForRaise = (function(initial, step) {
+                let salary = initial;
+                return () => {
+                    salary += step;
+                    return salary;
+                };
+            })(initial, step);
+            const raiseSalaryBy100 = _.before(count, askForRaise);
 
-        	const randomExcutedCount = count + Math.floor(Math.random()* 100),
-        		finalSalary = initial + step * count;
-        	const result = Array.from(new Array(randomExcutedCount).keys()).every(function(index){
-        		const curSalary = raiseSalaryBy100();
-        		if(index+1>=count) return finalSalary === curSalary;
-        		return curSalary === initial + step * (index+1);
-        	})
+            const randomExcutedCount = count + Math.floor(Math.random() * 100),
+                finalSalary = initial + step * count;
+            const result = Array.from(new Array(randomExcutedCount).keys()).every(function(index) {
+                const curSalary = raiseSalaryBy100();
+                if (index + 1 >= count) return finalSalary === curSalary;
+                return curSalary === initial + step * (index + 1);
+            })
 
-        	assert.equal(result,true)
+            assert.equal(result, true)
+        })
+    })
+
+    describe('4. _.partial 函数：', function() {
+
+        it('should pre-fill partial args and get final value', function() {
+            const add = (a, b) => a + b;
+            const add5 = _.partial(add, 5);
+            assert.equal(15, add5(10));
+        })
+        it('should remain un-filled when passing _ to the args', function() {
+            const selectMin = (...args) => Math.min.apply(null, args)
+            const selectMinArgs = _.partial(selectMin, _, 4, 5);
+            assert.equal(4, selectMinArgs(10));
+        })
+        it('accepts more arguments than the number of placeholders', function() {
+            const func = _.partial(function() {
+                return arguments.length
+            }, _, 'b', _, 'd');
+            assert.equal(5, func('a', 'c', 'e'));
+        })
+        it('cound remain arg length when some of the placeholders aren\'t fullfilled when calling', function() {
+            const func = _.partial(function() {
+                return arguments.length
+            }, _, 'b', _, 'd');
+            assert.equal(4, func('a'));
+        })
+        it('unfilled placeholders are undefined', function() {
+            const func = _.partial(function() {
+                return arguments[2]
+            }, _, 'b', _, 'd');
+            assert.equal(undefined, func('a'));
+        })
+    })
+
+    describe('5. _wrap函数 ', function() {
+        it('should wrap the given salutation function', function() {
+            let hello = name=> 'hello: ' + name;
+            hello = _.wrap(hello, function(func) {
+                return "before, " + func("moe") + ", after";
+            });
+            assert.equal(hello(),'before, hello: moe, after')
+        })
+
+        it('should hold fn as the first parameter of the wrapper', function() {
+            let hello = name=> 'hello: ' + name;
+            let wrappedHello = _.wrap(hello, function(func) {
+                assert.equal(hello,func)
+                return "before, " + func("moe") + ", after";;
+            });
+            wrappedHello()
+        })
+    })
+    describe('6. _memoize函数 ', function() {
+        const fib = n=> n < 2 ? n : fib(n - 1) + fib(n - 2);
+
+        it('should memoize the value of a fibonacci result and equal to it', function() {
+            const memoFib = _.memoize(fib);
+            assert.equal(fib(10),55)
+            assert.equal(memoFib(10),55)
+        })
+
+        it('should memoize all the result of the function', function() {
+            const  upper = _.memoize(s=>s.toUpperCase());
+            assert.strictEqual(upper('foo'),'FOO');
+            assert.strictEqual(upper('bar'),'BAR');
+            assert.deepEqual(upper.cache,{'foo':'FOO','bar':'BAR'},'cache return all the cached value')
+            assert.property(upper,'cache')
+        })
+
+
+        it('should store the hashed key as the cache key', function() {
+            var objCacher = _.memoize((value, key) =>({key: key, value: value}), 
+                                        (value, key) => key);
+            var myObj = objCacher('a', 'alpha');
+            var myObjAlias = objCacher('b', 'alpha');
+            assert.notStrictEqual(myObj, void 0, 'object is created if second argument used as key');
+            assert.strictEqual(myObj, myObjAlias, 'object is cached if second argument used as key');
+            assert.strictEqual(myObj.value, 'a', 'object is not modified if second argument used as key')
         })
     })
 })
